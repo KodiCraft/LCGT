1fe9ce2b3bbee316eb6bd1d03ea58a389957f06a
Kodi Craft <kodi@kdcf.me>
2023-07-29 15:36:27 +0200

    improved turn detection to now account for scale

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     win = tk.Tk()
     win.title("LCGT - Limbus Company Grind Tool")
 
-    bh = BattleHandler(1, False)
+    bh = BattleHandler(0.25, True)
 
     btn = tk.Button(win, text="Start", command=bh.do_battle)
     btn.grid(column=0, row=0)
