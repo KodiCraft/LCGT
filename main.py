2bf969336f89838a28988917737a7c7dd4152102
Kodi Craft <kodi@kdcf.me>
2023-07-29 12:18:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import asyncio
+
+
+def main():
+    win = tk.Tk()
+    win.title("LCGT - Limbus Company Grind Tool")
+
+    win.mainloop()
+
+
+if __name__ == '__main__':
+    main()
